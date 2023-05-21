96c91f5b36648377cf9de6341a1acf6ef06a50fd
JaswanthP6878 <klucse2000039034@gmail.com>
2023-05-21 17:06:02 +0530

    sorted responses based on queries

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"strings"
+
 	"greenlight.jaswanthp.com/internal/validator"
 )
 
@@ -19,4 +21,21 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
